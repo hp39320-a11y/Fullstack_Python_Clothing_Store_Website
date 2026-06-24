61cbe8d936a2c39ab03e4b643d56a9804f4a6e5d
hp39320-a11y <hp39320@gmail.com>
2026-06-24 09:33:23 +0530

    docs: add docstring to cart_count context processor

--- storeproject/storeapp/context_processors.py
+++ storeproject/storeapp/context_processors.py
@@ -1,6 +1,10 @@
 from .models import Cart
 
 def cart_count(request):
+    """
+    Context processor to calculate and provide the total number of items
+    in the current authenticated user's shopping cart globally to all templates.
+    """
     count = 0
     if request.user.is_authenticated:
         count = Cart.objects.filter(user=request.user).count()
