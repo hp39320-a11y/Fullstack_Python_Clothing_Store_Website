9d4007387373c4a3599c8d6ca9569d82f1e1129f
hp39320-a11y <hp39320@gmail.com>
2026-06-24 09:33:24 +0530

    docs: document custom user registration form

--- storeproject/storeapp/forms.py
+++ storeproject/storeapp/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegisterForm(UserCreationForm):
+    """
+    Custom registration form extending Django's built-in UserCreationForm
+    to request and capture user first name and last name during signup.
+    """
 
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
