defe5f657472a4dc3129ca8c30ecbc6cfd02b7be
hp39320-a11y <hp39320@gmail.com>
2026-06-25 10:34:29 +0530

    test: initialize unit tests for Category and Subcategory models

--- storeproject/storeapp/tests.py
+++ storeproject/storeapp/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Cateogry, Subcategory
 
-# Create your tests here.
+class CategorySubcategoryModelTest(TestCase):
+    def setUp(self):
+        self.category = Cateogry.objects.create(name="Men's Wear")
+        self.subcategory = Subcategory.objects.create(category=self.category, name="T-Shirts")
+
+    def test_category_creation(self):
+        self.assertEqual(self.category.name, "Men's Wear")
+        self.assertEqual(str(self.category), "Men's Wear")
+
+    def test_subcategory_creation(self):
+        self.assertEqual(self.subcategory.name, "T-Shirts")
+        self.assertEqual(self.subcategory.category, self.category)
+        self.assertEqual(str(self.subcategory), "Men's Wear - T-Shirts")
