44103d4e90a26e9649904ea3cf153dc2ee7487d1
hp39320-a11y <hp39320@gmail.com>
2026-06-25 10:42:38 +0530

    test: initialize adminpanel unit tests for order dashboard

--- storeproject/adminpanel/tests.py
+++ storeproject/adminpanel/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class AdminPanelDashboardTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.normal_user = User.objects.create_user(username="normaluser", password="password")
+        self.staff_user = User.objects.create_user(username="staffuser", password="password", is_staff=True)
+
+    def test_dashboard_blocks_anonymous(self):
+        url = reverse('admin_dashboard')
+        response = self.client.get(url)
+        # Should redirect to login since @login_required is used
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue('login' in response.url)
+
+    def test_dashboard_blocks_non_staff(self):
+        url = reverse('admin_dashboard')
+        self.client.login(username="normaluser", password="password")
+        response = self.client.get(url)
+        # Should redirect to index page because of @admin_required redirect('index')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/')
+
+    def test_dashboard_allows_staff(self):
+        url = reverse('admin_dashboard')
+        self.client.login(username="staffuser", password="password")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'adminpanel/dashboard.html')
