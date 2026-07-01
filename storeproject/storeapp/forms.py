8d7a8c9d682fc51723e3a35fb59878dad239ac94
hp39320-a11y <hp39320@gmail.com>
2026-07-01 10:06:00 +0530

    feat: define ReviewForm in forms.py

--- storeproject/storeapp/forms.py
+++ storeproject/storeapp/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Review
 
 class RegisterForm(UserCreationForm):
     """
@@ -14,3 +15,14 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name','last_name','username','email','password1','password2']
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'title', 'comment']
+        widgets = {
+            'rating': forms.Select(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Summarize your experience...'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 4, 'placeholder': 'Write your review here...'}),
+        }
